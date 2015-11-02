6b14d5ef5a0f387417a0594f6ad04cf040cca459
Ray Goodwin <rgoodwin16@outlook.com>
2015-11-02 14:50:26 -0500

    11/2/15
    Added
    -Blogpost model
    -Comment model

---
+++ RG_Personal/RG_Personal/Models/Code First/BlogPost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RG_Personal.Models
+{
+    public class Blogpost
+    {
+        public Blogpost()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public System.DateTime Created { get; set; }
+        public System.DateTime? Updated { get; set; }
+        public string Slug { get; set; }
+
+        [AllowHtml]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+
+        public string Category { get; set; }
+        public string MediaUrl { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ RG_Personal/RG_Personal/Models/Code First/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using RG_Personal.Models;
+
+namespace RG_Personal.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+        public System.DateTimeOffset Created { get; set; }
+        public System.DateTimeOffset? Updated { get; set; }
+        public string UpdateReason { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+        public virtual Blogpost Post { get; set; }
+
+    }
+}
