18667c3ad44c805818d18e0d77d9b37e8ac6302f
Ray Goodwin <rgoodwin16@outlook.com>
2015-11-18 10:11:40 -0500

    11/18/15
    Added
    -blogSvc
    -commentSvc
    Changed
    -index.html (added references to blogSvc and commentSvc

---
+++ RG_Personal/RG_Personal/App/services/blog/blogSvc.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    angular.module('rg_personal').factory('blogSvc', ['$http', function ($http) {
+        var f = {};
+
+        f.list = function () {
+            return $http.post("api/Blog/Index").then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.create = function (model) {
+            return $http.post("api/Blog/Create", model).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.details = function (id) {
+            return $http.post("api/Blog/Details?id=" + id).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.edit = function (model) {
+            return $http.post("api/Blog/Edit", model).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.delete = function (id) {
+            return $http.post("api/Blog/Delete?id=" + id).then(function (response) {
+                return response.data;
+            })
+        }
+
+        return f;
+    }])
+})();
---
+++ RG_Personal/RG_Personal/App/services/comment/commentSvc.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    angular.module('rg_personal').factory('commentSvc', ['$http', function ($http) {
+        var f = {};
+
+        f.list = function () {
+            return $http.post("api/Comments/Index").then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.create = function (model) {
+            return $http.post("api/Comments/Create", model).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.details = function (id) {
+            return $http.post("api/Comments/Details?id=" + id).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.edit = function (model) {
+            return $http.post("api/Comments/Edit", model).then(function (response) {
+                return response.data;
+            })
+        }
+
+        f.delete = function (id) {
+            return $http.post("api/Comments/Delete?id=" + id).then(function (response) {
+                return response.data;
+            })
+        }
+
+        return f;
+    }])
+})();
